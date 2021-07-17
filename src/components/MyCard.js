7c62d35709a7110916c8b8b2de439ea7b631f19e
Abhishek Saraswat <77572876+Abhisheksaraswat21@users.noreply.github.com>
2021-07-17 17:51:22 +0530

    Update MyCard.js

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -20,15 +20,14 @@ const MyCard = ({ match }) => {
   const [detail, setDetail] = useState({});
 
   const [open, setopen] = useState(false);
-  //ye dialogue box open close hone ke liye banaya ja rha hai ...by default close hoga
 
   const handleClick = (id) => {
     getMatchDetail(id)
       .then((data) => {
         console.log("MATCH DATA", data);
 
-        setDetail(data); //button click hote hai isme match ki details aajayengi
-        handleOpen(); //isse jb handleclick chalega tb ye handle open run krega
+        setDetail(data); 
+        handleOpen(); 
       })
 
       .catch((error) => console.log(error));
@@ -75,7 +74,7 @@ const MyCard = ({ match }) => {
               Show Details
             </Button>
             <Button
-              style={{ marginLeft: 5 }} //hr button ke left me thoda sa space aayga
+              style={{ marginLeft: 5 }} 
               variant="outlined"
               color="primary"
             >
@@ -124,20 +123,8 @@ const MyCard = ({ match }) => {
       </DialogActions>
     </Dialog>
   );
-  /* material ui se dialog banaya hai yha pe 
-   <Dialog open={open} onClose={handleClose}>  close hone pe ek funcitoin aur chalaya hai
-  //by default open krna hai to open ki jagah true llikho...wrna open hi likho
-
-     <Typography>{detail.stat}</Typography> isse hume match ka status milega...ye stat api me pehle se isi naam se dia tha..to dekh skte 
-     ho console me ek baar
-
-{detail.matchStarted ? "Match Started" : "Match not Started"}
-matchstarted bhi api me use kia hua hai...agr ye true hai  to hum Match stated likhenge
   
-{detail.score}  ye score print krne ke liye hai
 
-
-*/
 
   return (
     <fragment>
@@ -149,48 +136,10 @@ matchstarted bhi api me use kia hua hai...agr ye true hai  to hum Match stated l
 
 export default MyCard;
 
+
+
 /*
-<Card>
 
-<CardContent>
-    <Typography>hs is a card</Typography>
-</CardContent>
-
-</Card>
-ye saare ui component hai
-
-{new Date().toString()} iise abhi ka datee and time aagya
-
-card ke andar data ke liye card content use krna sahi hai
-
-
-grid se hum log cheezo ko parallel le aarhe hai 
-
-
-
-const MyCard = ({ match }) => { 
-  is trh se match likhne se match ki value app.js se jo humne ek key ki trh pass 
-  ki thi vese aagai yha pe 
-
-
-
-
-  {match["team-1"]}
-  ye app . js se aarha hai....ye bascically har match me team1 ka naam hai...se hi chalega ye loop
-  hr araaay me team1 me konnsa naam hai wo value
-
-
- Start Time{new Date(match.dateTimeGMT).toLocaleString()}
- isme match to hume app.js se aarha hain data ...agr console me dekhe to har matdch me ek 
- variable hia dateTimeGmt jisme time se related data hai...to usko ye liya yha aur local strting me convert krdia
-
-
- onClick={() => {
-                handleClick(match.unique_id);
-                show detail waale button me hum log is function ko paas krenge jisme hum hr match ki unique id use krrhe hai
-
-upar handleclick me hum log getMatchDetail(id) is functon me id paas krrhe hai...ye promise dega to then aur catch use krenge
-
-
+MAXIMUM THINGS HAVE BEEN CREATED USING MATERIAL UI
 
 */
